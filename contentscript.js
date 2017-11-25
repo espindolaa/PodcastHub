289301c9976cab4638c2266833a832958a16c114
Lucas Espindola <lucas.espindola@hotmail.com>
2017-11-24 23:13:55 -0200

    Non-static podcast

--- contentscript.js
+++ contentscript.js
@@ -1,12 +1,11 @@
-// Avoid recursive frame insertion...
-var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
-if (!location.ancestorOrigins.contains(extensionOrigin)) {
-    var iframe = document.createElement('iframe');
-    // Must be declared at web_accessible_resources in manifest.json
-    iframe.src = chrome.runtime.getURL('frame.html');
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("but").addEventListener("click", handler);
+});
 
-    // Some styles for a fancy sidebar
-    iframe.style.cssText = 'position:fixed;top:0;left:0;display:block;' +
-                           'width:300px;height:100%;z-index:1000;';
-    document.body.appendChild(iframe);
+// The handler also must go in a .js file
+function handler() {
+  var link =document.getElementById("link").value;
+  window.alert(link);
+  var iframe = document.getElementById("frame") 
+  iframe.src = link; 
 }
