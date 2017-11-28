1efee34e9c20f2fa7e9860b324753b7e759a4beb
Eduardoschk <eduardoschmittk@gmail.com>
2017-11-27 23:51:45 -0200

    regex

--- contentscript.js
+++ contentscript.js
@@ -5,7 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 // The handler also must go in a .js file
 function handler() {
   var link =document.getElementById("link").value;
-  window.alert(link);
+  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+  var match = link.match(regExp);
+  var result =  (match&&match[7].length==11)? match[7] : false;
+  window.alert(result);
   var iframe = document.getElementById("frame") 
-  iframe.src = link; 
+  iframe.src = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + result + '" frameborder="0"></iframe>'; 
 }
+
+// <iframe width="560" height="315" src="https://www.youtube.com/embed/mvT7wws6eAU" frameborder="0"></iframe>
