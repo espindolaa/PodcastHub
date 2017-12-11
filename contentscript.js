57e593d0d6c7321830faa5a5b177927b6ba2ceef
Lucas Espindola <lucas.espindola@hotmail.com>
2017-12-11 01:02:48 -0200

    JQuery appearing at the DOM page, dinamically

--- contentscript.js
+++ contentscript.js
@@ -2,15 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("but").addEventListener("click", handler);
 });
 
-// The handler also must go in a .js file
 function handler() {
   var link =document.getElementById("link").value;
   var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
   var match = link.match(regExp);
   var result =  (match&&match[7].length==11)? match[7] : false;
-  window.alert(result);
-  var iframe = document.getElementById("frame") 
-  iframe.src = "https://www.youtube.com/embed/" + result; 
-}
+  //window.alert(result);
+  //var iframe = document.getElementById("frame");
 
-//<iframe width="560" height="315" src="https://www.youtube.com/embed/mvT7wws6eAU" frameborder="0"></iframe>
+  chrome.tabs.executeScript(null, { file: "jquery.js" });
+  chrome.tabs.executeScript(null, { file: "jquery-ui.js" });
+  chrome.tabs.insertCSS(null, { file: "jquery-ui.css"});
+  chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+  chrome.tabs.executeScript(activeTabs[0].id, { code: 'var x = document.createElement("div"); x.id="dialog"; x.innerHTML = "\
+  <iframe id=%22frame%22 width=%22560%22 height=%22315%22 src=https://www.youtube.com/embed/YtWjqtlZldM frameborder=%220%22 allowfullscreen></iframe>";\
+  document.body.appendChild(x); $("#dialog").dialog();' });
+    });
+}    
+  
