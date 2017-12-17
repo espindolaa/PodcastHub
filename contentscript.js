ffb5741dcf79d72aa805a46e22cc12d7a1c856ee
Lucas Espindola <lucas.espindola@hotmail.com>
2017-12-17 12:27:21 -0200

    Some cleanup on the code

--- contentscript.js
+++ contentscript.js
@@ -7,12 +7,11 @@ function handler() {
   var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
   var match = link.match(regExp);
   var result =  (match&&match[7].length==11)? match[7] : false;
-  //window.alert(result);
-  //var iframe = document.getElementById("frame");
 
   chrome.tabs.executeScript(null, { file: "jquery.js" });
   chrome.tabs.executeScript(null, { file: "jquery-ui.js" });
   chrome.tabs.insertCSS(null, { file: "jquery-ui.css"});
+  
   chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
   chrome.tabs.executeScript(activeTabs[0].id, { code: 'var x = document.createElement("div"); x.id="dialog"; x.innerHTML = "\
   <iframe id=%22frame%22 width=%22560%22 height=%22315%22 src=https://www.youtube.com/embed/'+result+' frameborder=%220%22 allowfullscreen></iframe>";\
