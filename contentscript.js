2f80a78beedff576533e9688c0305aa20288af06
Eduardoschk <eduardoschmittk@gmail.com>
2017-12-24 13:46:08 -0200

    tratativa de links, obrigatorio ter youtube.com/ e watch?v=

--- contentscript.js
+++ contentscript.js
@@ -6,20 +6,23 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function handler() {
-  var link =document.getElementById("link").value;
-  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-  var match = link.match(regExp);
-  var result =  (match&&match[7].length==11)? match[7] : false;
-
-  chrome.tabs.query({ active: true, currentWindow: true}, (activeTabs) => {
-    chrome.tabs.executeScript(activeTabs[0].id, { 
-        code: 'var x = document.createElement("div");\
-        x.id="dialog"; \
-        x.innerHTML = "<iframe id=%22frame%22 width=96% height=96% src=https://www.youtube.com/embed/'+result+' allowfullscreen></iframe>";\
-        document.body.appendChild(x);\
-        $("#dialog").dialog({  beforeClose: function( event, ui ) { $("#dialog").empty();  $("#dialog").remove();}});' 
-      });
-  });
-
+  var link = document.getElementById("link").value;
+  if (link.includes("youtube.com/")&&link.includes("watch?v=")) {
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = link.match(regExp);
+    var result = (match&&match[7].length==11)? match[7] : false;
+  
+    chrome.tabs.query({ active: true, currentWindow: true}, (activeTabs) => {
+      chrome.tabs.executeScript(activeTabs[0].id, { 
+          code: 'var x = document.createElement("div");\
+          x.id="dialog"; \
+          x.innerHTML = "<iframe id=%22frame%22 width=96% height=96% src=https://www.youtube.com/embed/'+result+' allowfullscreen></iframe>";\
+          document.body.appendChild(x);\
+          $("#dialog").dialog({  beforeClose: function( event, ui ) { $("#dialog").empty();  $("#dialog").remove();}});' 
+        });
+    });
+  } else {
+    alert("URL incompativel com videos do YouTube");
+  }
 }    
   
